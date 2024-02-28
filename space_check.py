c7133ad03cbb90c667e351f9893a4efe562b1346
wangqinglin <chd_wql@qq.com>
2024-02-28 13:41:39 +0800

    增加数据解析，直接输出到excel

--- space_check.py
+++ space_check.py
@@ -26,8 +26,10 @@ def read_config(filename):
         # SSH连接和执行命令
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        print(f'checking host {host}...')
         ssh.connect(hostname=host, port=22, username=user, password=password)
-        stdin, stdout, stderr = ssh.exec_command('df -h')
+        # stdin, stdout, stderr = ssh.exec_command("df -h --total | grep '^total'")
+        stdin, stdout, stderr = ssh.exec_command("[ -d '/usr/local/bin' ] && /usr/local/bin/df -h --total | grep '^total' || df -h --total | grep '^total'")
         res, err = stdout.read(), stderr.read()
         result = res if res else err
         ssh.close()
@@ -36,13 +38,3 @@ def read_config(filename):
         results[section] = result.decode()
     
     return results
-
-# 从配置文件中读取参数并执行SSH命令，并输出结果
-config_file = 'config.ini'
-results = read_config(config_file)
-
-# 遍历结果并打印
-for section, result in results.items():
-    print(f"--- {section} ---")
-    print(result)
-    print()
